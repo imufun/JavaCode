15b57e648916c9ec55c31d8c591a3932b150f771
Imran <imufun1@gmail.com>
2015-11-08 23:32:33 +0600

    java code..

---
+++ src/javapractice/JavaPractice.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javapractice;
+
+/**
+ *
+ * @author Imran
+ */
+public class JavaPractice {
+
+    static void mainclass(String name, int roll) {
+        name = "imran";
+        roll = 10;
+
+    }
+
+    public static void main(String[] args) {
+        JavaPractice.mainclass("imran", 10);
+         System.out.println("mainclass");
+    }
+
+}
